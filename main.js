2c3faddb256de4d4e81b4a2fe61606168a770ef5
MaruVadala89 <maruvad15@gmail.com>
2023-08-10 18:14:18 -0300

    Codigo

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
-function AdondeVamos () {
+function ADondeVamos () {
 
 do {
     const comida = prompt ("¿Que comida te haria feliz hoy?");
     let restaurante;
+
+    if (comida === "sushi") {
+        restaurante = "Tokin"; }
+
+    else if (comida === "pasta") {
+        restaurante = "San Pietro"}
+
+    else if (comida === "pizza") {
+        restaurante ="Il Forno de Marco";}
+
+    else if (comida === "comida peruana") {
+        restaurante = "Sabores del peru"}
     
+    else {
+        alert ("Ups, No conozco un restaurante para esa comida, proximamente cargaremos nuevos lugares para tu deleite") }
+
+    } while (!restaurante);
+
+    console.log ("Te recomiendo ir a ${restaurante} para comer la/el mejor ${comida}")
+
+    ADondeVamos ()
 
 
 
@@ -12,11 +32,6 @@ do {
 
 
 
-
-
-
-
-} while
 
 
 
