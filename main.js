52ddfae8a4fbf51a2d83e8c62e3088ed2ed980ef
MaruVadala89 <maruvad15@gmail.com>
2023-08-21 12:15:15 -0300

    cambio

--- main.js
+++ main.js
@@ -2,43 +2,36 @@ function ADondeVamos() {
     let restaurante;
     let comida = prompt("¿Que comida te haria feliz hoy? Las opciones son:\n - Sushi \n - Pasta\n - Pizza\n - Ceviche\n - Ramen\n - Ramen");
 
-    do {
 
-        if (comida === null) {
+    comida = comida.toLowerCase();
 
-            alert("Cancelaste el pedido de recomendacion"); return;
+    if (comida === null) {
 
-        }
+        alert("Cancelaste el pedido de recomendacion");
+        return;
+    }
 
-        else if (comida === "sushi") {
-            restaurante = "Tokin";
-        }
-
-        else if (comida === "pasta") {
+    switch (comida) {
+        case "sushi":
+            restaurante = "tokin"
+            break;
+        case "pasta":
             restaurante = "San Pietro"
-        }
-
-        else if (comida === "pizza") {
-            restaurante = "Il Forno de Marco";
-        }
-
-        else if (comida === "ceviche") {
-            restaurante = "Sabores del peru";
-        }
-
-        else if (comida === "ramen") {
-            restaurante = "Nakama";
-        }
-
-        else if (comida === "empanadas") {
-            restaurante = "La Vieja Esquina";
-        }
-
-        else {
-            alert("No conozco un restaurante para esa comida, próximamente cargaremos nuevos lugares"); return;
-        }
-
-    } while (!restaurante);
+            break;
+        case "pizza":
+            restaurante = "Il Forno di Marco"
+            break;
+        case "ceviche":
+            restaurante = "Sabores del Peru"
+            break;
+        case "ramen":
+            restaurante = "Nakama"
+            break;
+        case "empanadas":
+            restaurante = "La Vieja Esquina"
+            break;
+        default:
+            alert("No conozco un restaurante para esa comida, próximamente cargaremos nuevos lugares"); return; }
 
 
     alert("Te recomiendo ir a " + restaurante + " para comer: " + comida); return;
